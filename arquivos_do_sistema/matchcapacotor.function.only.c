7c00c3f370a0b94d1b5d4b9710eb1b2b134b28f2
pitocoofc <ismaelkelvyn328@gmail.com>
2026-03-07 22:06:34 -0300

    Create matchcapacotor.function.only.c

---
+++ arquivos_do_sistema/matchcapacotor.function.only.c
@@ -0,0 +1,16 @@
+{
+  "name": "ndj-lib-reborn",
+  "version": "1.0.0",
+  "dependencies": {
+    "@capacitor/android": "^5.0.0",
+    "@capacitor/core": "^5.0.0",
+    "@capacitor/camera": "^5.0.0",
+    "@capacitor/haptics": "^5.0.0"
+  },
+  "devDependencies": {
+    "@capacitor/cli": "^5.0.0"
+  },
+  "scripts": {
+    "build": "echo 'No build step needed'"
+  }
+}
